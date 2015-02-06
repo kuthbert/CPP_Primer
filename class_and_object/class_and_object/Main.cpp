bdabdd672f10061f11533902c63e9bf38b627b46
XU ZAN <zan.xu@hella.com>
2015-02-06 16:08:39 +0800

    創建一個 "class_and_object" 的 Visual Studio C++ 解決方案/項目，  用來研究C++類的定義與實現， 學習對象的使用。

---
+++ class_and_object/class_and_object/Main.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************************************************************
+ * Project			: class_and_object
+ * Description		: Study how to define the class and use objects in C++ programming.
+ * ==========================================================================================================================
+ * File name		: Main.cpp
+ * Description		: This file implements the main() entry function for "class_and_object" projecct.
+ * Creator			: XU ZAN
+ * Creation date	: Fri.	Feb. 06, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ****************************************************************************************************************************/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+/******************************/
+	cout<<endl;
+	return 0;
+}
