b55f652fe25ac15aefb200e965e4c0c3c06fb1de
XU ZAN <zan.xu@hella.com>
2015-02-06 16:18:49 +0800

    Add Test.cpp and Test.hh program files.
    They are used to design and perform all test cases for "class_and_object"  Visual Studio C++ project.

---
+++ class_and_object/class_and_object/Test.cpp
@@ -0,0 +1,15 @@
+/*********************************************************************************************
+ * File name		: Test.cpp
+ * Description		: Perform all test cases in this file.
+ * Creator			: XU ZAN
+ * Creation date	: Fri.	Feb. 06, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ *********************************************************************************************/
+
+
+#include "Test.h"
+
+void Perform_General_Test_Cases(void)
+{
+}
