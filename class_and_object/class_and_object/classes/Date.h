8c6ed3f4ad1875b3afdc2c57b7206337f7f5acce
XU ZAN <zan.xu@hella.com>
2015-02-09 16:15:59 +0800

    常量型取值函數getter起到不改變私有數據成員的目的
    從Date對象轉換到CustomDate對象的成員轉換函數，使用取值函數或賦值函數來訪問私有數據成員的方法。

--- class_and_object/class_and_object/classes/Date.h
+++ class_and_object/class_and_object/classes/Date.h
@@ -13,7 +13,7 @@
 
 	#define GETTER_CONST
 
-
+	#include "CustomDate.h"
 
 	class Date;
 
@@ -32,25 +32,14 @@
 	#if !defined (GETTER_CONST)
 		int getyear(void);
 	#else	
-		int getyear(void) const
-		/* 注意：
-		 *
-		 * getter函數聲明為常量型，這樣可以保證該成員函數不會修改調用它的對象。
-		 * 通過加上const修飾， 可以使訪問對象數據的成員函數僅僅完成不會引起數據變動的那些操作，如
-		 * 返回用戶需要的數據，顯示數據，等等。
-		 *
-		 * 而且，如果某個Date對象被聲明為常量的話，那麽該對象不得調用任何非常量型成員函數，而不論
-		 * 這些函數是否真的試圖修改對象的數據。
-		 * 只有把那些不會引起數據改變的函數聲明為常量型，才能允許常量對象調用這些函數。
-		 */
-		{
-			return yr;
-		}
+		int getyear(void) const;
 	#endif
 		/*
 		 * A memmber function to set the year
 		 */
 		void setyear(int y);
+
+		operator CustomDate(void) const;
 	};
 
 #endif	/*  DATE_H  */
