656546ec4ff378b3c918dda00f3ec61afaa473db
XU ZAN <zan.xu@hella.com>
2015-03-11 19:12:14 +0800

    複製構造函數， 怎樣實現和調用“複製構造函數”。
    Date(const Date&);	// Copy constructor
    Date lastday(birthday);

--- class_and_object/destructor_operator/destructor/Date.h
+++ class_and_object/destructor_operator/destructor/Date.h
@@ -11,8 +11,13 @@
 #ifndef DATE_H
 #define DATE_H
 
+// #define ASSIGNMENT_OPERATOR_OVERLOAD
+// #define NEW_DELETE_OPERATOR
+#define COPY_CONSTRUCTOR
+
 	class Date
 	{
+#if defined (ASSIGNMENT_OPERATOR_OVERLOAD)
 	private:
 		int mo;
 		int da;
@@ -32,6 +37,30 @@
 		Date& operator=(const Date&);
 	#endif
 		void month_display(void);
+#endif	/*  ASSIGNMENT_OPERATOR_OVERLOAD  */
+
+#if defined (NEW_DELETE_OPERATOR)
+	private:
+		int yr;
+		int mo;
+		int da;
+	public:
+		Date(void);
+		~Date(void);
+#endif	/*  NEW_DELETE_OPERATOR  */
+
+#if defined (COPY_CONSTRUCTOR)
+	private:
+		int yr;
+		int mo;
+		int da;
+		char *month;
+	public:
+		Date(int y=0, int m=0, int d=0);
+		~Date(void);
+		Date(const Date&);	// Copy constructor
+		void display(void) const;
+#endif	/*  COPY_CONSTRUCTOR  */
 	};
 
 #endif	/*  DATE_H  */
---
+++ class_and_object/destructor_operator/destructor/Test_Date.h
@@ -0,0 +1,16 @@
+/********************************************************************************************
+ * File name		: Test_Date.h
+ * Description		: Design test cases to verify the class "Date".
+ * Creator			: XU ZAN
+ * Creation date	: Mon.	March 09, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ********************************************************************************************/
+
+
+#ifndef TEST_DATE_H
+#define TEST_DATE_H
+
+	void TestCase4_How2UseCopyConstructor(void);
+
+#endif	/*  TEST_DATE_H  */
