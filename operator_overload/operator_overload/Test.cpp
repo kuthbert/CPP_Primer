44345e1d3b331c808792a853a4e3317ddd06c86a
XU ZAN <zan.xu@hella.com>
2015-03-16 15:17:48 +0800

    研究C++類的重載運算符功能與特性。

---
+++ operator_overload/operator_overload/Test.cpp
@@ -0,0 +1,17 @@
+/********************************************************************************************
+ * File name		: Test.cpp
+ * Description		: Perform all test cases here for current project.
+ * Creator			: Frederique Hsu
+ * Creation date	: Thur.	March 12, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ********************************************************************************************/
+
+#include "Test.h"
+#include "operator_overload.h"
+
+void Perform_General_Test_Cases(void)
+{
+	/* Insert your test cases here */
+	Perform_TestCase4_OperatorPlusOverload();
+}
