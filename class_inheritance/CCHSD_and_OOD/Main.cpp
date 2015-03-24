4145183478e4a98ba0daf9e46702013e05d80a19
Frédérick Hsu <zan.xu@hella.com>
2015-03-24 15:49:36 +0800

    為 CCHSD_and_OOD.vcproj  項目添加第一個程序文件 Main.cpp， 實現項目的main()函數。

---
+++ class_inheritance/CCHSD_and_OOD/Main.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************************************************
+ * Project name		: CCHSD_and_OOD
+ * Description		: 学习研究 "复杂的类层次结构设计" 与 "纯面向对象的设计".
+ *					  Complicated Class Hierarchy Structure Design  (abbr. CCHSD)  and 
+ *					  Object-oriented design (abbr. OOD)
+ * ==============================================================================================================
+ * File name		: Main.cpp
+ * Description		: This file will implement the main() portal function for "CCHSD_and_OOP.vcproj" project.
+ * Creator			: Frederick Hsu
+ * Creation date	: Tue.	March 24, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ****************************************************************************************************************/
+
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+
+/******************************/
+	std::exit(0);
+}
