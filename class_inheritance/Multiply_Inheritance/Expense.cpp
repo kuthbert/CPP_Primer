cc3295ae5e639acf24458e1414397d9ad31797a4
xuzan <zan.xu@hella.com>
2015-03-31 14:22:22 +0800

    Create and implement another 2nd hierarchical layer of derived class "Expense", it is publicly inherited from direct base class "Property", indirect root base class "DisplayObject".

---
+++ class_inheritance/Multiply_Inheritance/Expense.cpp
@@ -0,0 +1,20 @@
+/**********************************************************************************************************************
+ * File name		: Expense.cpp
+ * Description		: Implement the new class "Expense", derived from base class "Property".
+ * Creator			: Frederick Hsu
+ * Creation date	: Tue.	March 31, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ **********************************************************************************************************************/
+
+#include "Expense.h"
+
+Expense::Expense(int id) : Property(id)
+{
+	/* empty */
+}
+
+Expense::~Expense(void)
+{
+	/* empty */
+}
