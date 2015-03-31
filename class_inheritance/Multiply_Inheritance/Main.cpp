f7098bd3b1ec48dd6fe7d54d036bae618ca76fda
xuzan <zan.xu@hella.com>
2015-03-31 09:05:05 +0800

    Create a new project : Multiply_Inheritance.vcproj under the solution "class_inheritance.sln"
    To study the multiply inheritance feature of object-oriented C++ programming.

---
+++ class_inheritance/Multiply_Inheritance/Main.cpp
@@ -0,0 +1,24 @@
+/********************************************************************************************
+ * Project name		: Multiply_Inheritance
+ * Description		: Study the "Multiply inheritance" technology in C++ programming.
+ * ==========================================================================================
+ * File name		: Main.cpp
+ * Description		: This file will implement the main() portal function.
+ * Creator			: Frederick Hsu
+ * Creation date	: Mon.	March 30, 2015
+ * Copyright(C)		2015	All rights reserved.
+ *
+ ********************************************************************************************/
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+
+
+/******************************/
+	std::cout<<std::endl;
+	std::exit(0);
+}
