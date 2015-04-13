f3f050a3c835c145f94ed5f7a74525a94903780b
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-04-13 21:58:28 +0800

    Add 2 program files : Test_BArray.cpp and Test_BArray.h

---
+++ class_function_template/class_function_template/Test_BArray.cpp
@@ -0,0 +1,10 @@
+//
+//  Test_BArray.cpp
+//  class_function_template
+//
+//  Created by Frédérick Hsu on 15/4/12.
+//  Copyright (c) 2015年 Frédérick Hsu. All rights reserved.
+//
+
+#include "Test_BArray.h"
+#include "barray.h"
