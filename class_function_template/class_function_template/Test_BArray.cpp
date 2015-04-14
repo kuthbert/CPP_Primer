b0affc9779e98a6ebf8bb31ae5ac4d6c191129cb
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-04-15 01:16:31 +0800

    研究观察当超出 数组模版的下标时，用assert宏来弹出数组越界的运行时错误，阻止程序继续往下运行。
    以此来保证程序的安全。

--- class_function_template/class_function_template/Test_BArray.cpp
+++ class_function_template/class_function_template/Test_BArray.cpp
@@ -1,10 +1,47 @@
-//
-//  Test_BArray.cpp
-//  class_function_template
-//
-//  Created by Frédérick Hsu on 15/4/12.
-//  Copyright (c) 2015年 Frédérick Hsu. All rights reserved.
-//
+/******************************************************************************************
+ * File name        : Test_BArray.cpp
+ * Description      : 測試“使用有界數組模板”的情形。
+ * Creator          : Frederick Hsu
+ * Creation date    : Wed.  April 15, 2015
+ * Copyright(C)     2015    All rights reserved.
+ *
+ ******************************************************************************************/
+
 
 #include "Test_BArray.h"
 #include "barray.h"
+#include "Date.h"
+
+
+
+void TestCase4_BoundedArrayTemplate(void)
+{
+    /* A bounded array of dates */
+    Array<Date, 5> dateArray;
+    
+    /* Some date objects */
+    Date dt1(1999, 12, 17), dt2(2000, 11, 30), dt3(2010, 6, 24),
+         dt4(2012, 10, 31), dt5(2014, 8, 5);
+    
+    /* Put the date objects in the array */
+    dateArray[0] = dt1;
+    dateArray[1] = dt2;
+    dateArray[2] = dt3;
+    dateArray[3] = dt4;
+    dateArray[4] = dt5;
+    
+    /* Display these dates */
+    for (int i=0; i<5; i++)
+    {
+        std::cout<<dateArray[i]<<std::endl;
+    }
+    std::cout<<std::flush;
+    
+    /* Try to put an additional date into the array
+     * outside the range of the subscript.
+     */
+    Date dt6(2015, 4, 15);
+    dateArray[5] = dt6;     // Template's assertion aborts.
+    
+    return;
+}
