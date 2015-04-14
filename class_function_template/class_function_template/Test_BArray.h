03ca01b65c1b17fa21af2a38668321a1af6a065e
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-04-15 01:12:36 +0800

    Declare a function prototype : void TestCase4_BoundedArrayTemplate(void);

--- class_function_template/class_function_template/Test_BArray.h
+++ class_function_template/class_function_template/Test_BArray.h
@@ -1,13 +1,15 @@
-//
-//  Test_BArray.h
-//  class_function_template
-//
-//  Created by Frédérick Hsu on 15/4/12.
-//  Copyright (c) 2015年 Frédérick Hsu. All rights reserved.
-//
+/******************************************************************************************
+ * File name        : Test_BArray.h
+ * Description      : 測試“使用有界數組模板”的情形。
+ * Creator          : Frederick Hsu
+ * Creation date    : Wed.  April 15, 2015
+ * Copyright(C)     2015    All rights reserved.
+ *
+ ******************************************************************************************/
 
-#ifndef class_function_template_Test_BArray_h
-#define class_function_template_Test_BArray_h
+#ifndef TEST_BARRAY_H
+#define TEST_BARRAY_H
 
+    void TestCase4_BoundedArrayTemplate(void);
 
-#endif
+#endif  /*  TEST_BARRAY_H  */
