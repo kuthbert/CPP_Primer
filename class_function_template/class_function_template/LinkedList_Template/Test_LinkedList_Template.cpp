41427c1603754c1b0ba097b8385021229845c348
徐贊 (Frédérick Hsu) <frederick.hsu@icloud.com>
2015-04-20 00:47:17 +0800

    Add 2 program files "Test_LinkedList_Template.h " and "Test_LinkedList_Template.cpp"  to validate the template running mechanism.

---
+++ class_function_template/class_function_template/LinkedList_Template/Test_LinkedList_Template.cpp
@@ -0,0 +1,16 @@
+/*********************************************************************************************************
+ * File name        : Test_LinkedList_Template.cpp
+ * Description      : Design the test cases to verify the LinkedList class template.
+ * Created          : Frédérick Hsu
+ * Creation date    : Sun.  April 19, 2015
+ * Copyright (c)    2015 Frédérick Hsu. 
+ * All rights reserved.
+ *
+ *********************************************************************************************************/
+
+
+#include "Test_LinkedList_Template.h"
+
+#include "LinkedList.h"
+
+
---
+++ class_function_template/class_function_template/LinkedList_Template/Test_LinkedList_Template.h
@@ -0,0 +1,17 @@
+/*********************************************************************************************************
+ * File name        : Test_LinkedList_Template.h
+ * Description      : Design the test cases to verify the LinkedList class template.
+ * Created          : Frédérick Hsu
+ * Creation date    : Sun.  April 19, 2015
+ * Copyright (c)    2015 Frédérick Hsu. 
+ * All rights reserved.
+ *
+ *********************************************************************************************************/
+
+
+#ifndef TEST_LINKEDLIST_TEMPLATE
+#define TEST_LINKEDLIST_TEMPLATE
+
+
+
+#endif  /*  TEST_LINKEDLIST_TEMPLATE  */
